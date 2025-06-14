87c20c5ec3a0d3eb01e13b0a721611665f03539c
Isabely Lourenço <isabelylourencodev@gmail.com>
2025-06-14 14:25:49 -0300

    Update batalhaNaval.c
    Criação do código que exibi o tabuleiro e dois navios.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -9,32 +9,90 @@ int main() {
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    //Criação da matriz do tabuleiro
+    int tabuleiro[10][10] = {
+        {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0,0}
+    };
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    //Criação dos navios
+    int navio1[3] = {3,3,3}; // navio na vertical
+    int navio2[3] = {3,3,3};
+
+
+    int vertical_navio1 = 1;
+    int horizontal_navio1 = 1;
+    int vertical_navio2 = 2;
+    int horizontal_navio2 = 5;
+
+
+    //Impressão do tabuleiro
+    printf("--- Tabuleiro Batalha Naval ---\n\n");
+    printf("  ");
+
+  
+    for (char c = 'A'; c < 'A' + 10; c++) {
+        printf(" %c ", c);
+    }
+    printf("\n");
+
+    //Impressão dos navios
+
+    if (vertical_navio1 <= 10)
+    {
+        if (horizontal_navio1 + 3 <= 10)
+        {
+           for (int i = 0; i < 3; i++) {
+                tabuleiro[vertical_navio1 + i][horizontal_navio1] = navio1[i];
+            } 
+        } else {
+            printf("Erro na impressão do navio por ultrapassar o limite do tabuleiro.");
+        }
+        
+    } else {
+            printf("Erro na impressão do navio por ultrapassar o limite do tabuleiro.");
+        }
+
+
+    if (horizontal_navio2 <= 10)
+    {
+        if (horizontal_navio2 + 3 <= 10)
+        {
+           for (int i = 0; i < 3; i++) {
+                tabuleiro[vertical_navio2][horizontal_navio2 + i] = navio1[i];
+            } 
+        } else {
+            printf("Erro na impressão do navio por ultrapassar o limite do tabuleiro.");
+        }
+        
+    } else {
+            printf("Erro na impressão do navio por ultrapassar o limite do tabuleiro.");
+        }
+    
+    
+
+    //Loop que imprime o tabuleiro
+    for (int i = 0; i < 10; i++)
+    {   
+        //impressão de números para representar as linhas
+        printf("%d", 1 + i);
+        for (int j = 0; j < 10; j++)
+        {
+            printf("  %d", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n\n");
+    printf("Coordenadas do navio 1 : x = %d, y = %d\n", (horizontal_navio1 + 1), vertical_navio1 + 1);
+    printf("Coordenadas do navio 2 : x = %d, y = %d", (horizontal_navio2 + 1), vertical_navio2 + 1);
+
+    
 
     return 0;
 }
+
